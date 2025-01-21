f56fc475a59c7267eb200ff05c96fd795385a9db
StenioEric <stenioeric@gmail.com>
2025-01-21 09:36:28 -0300

    Estruturação da MovieList e implementação da API

---
+++ src/components/MovieList/index.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import "./index.scss";
+import axios from "axios";
+import { Movie } from "@/type";
+import MovieCard from "../MovieCard";
+
+export default function MovieList() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const getMovies = () => {
+    axios({
+      method: "get",
+      url: "https://api.themoviedb.org/3/discover/movie",
+      params: {
+        api_key: "4e1ab5dc8540ca07b5002ed46eb494e7",
+        language: "pt-BR",
+      },
+    }).then((response) => {
+      setMovies(response.data.results);
+    });
+  };
+
+  return (
+    <ul className="movie-list">
+      {movies.map((movie) => 
+        <MovieCard movie={movie}/>
+      )}
+    </ul>
+  );
+}
