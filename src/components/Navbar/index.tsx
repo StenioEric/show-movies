038ef28b12fd0a31c5798590a20e22bfb7631a43
StenioEric <stenioeric@gmail.com>
2025-01-21 09:37:28 -0300

    Customização da NavBar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import './index.scss'
 export default function Navbar() {
   return(
     <nav className="navbar">
-        <h1 className="page-title">Filmes</h1>
+        <h1 className="page-title">ShowFilmes</h1>
     </nav>
   );
 }
