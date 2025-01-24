f04903fc4d67d1bc5f4d4856a6139a258e49b1dc
StenioEric <stenioeric@gmail.com>
2025-01-23 23:40:06 -0300

    Adicionado Link que terá funcionalidade como botão e sua customização

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,14 @@
 import './index.scss'
+import Link from 'next/link';
 
 export default function Navbar() {
   return(
     <nav className="navbar">
         <h1 className="page-title">ShowFilmes</h1>
+
+        <Link href="/" className="btn-default">
+           Home
+       </Link>
     </nav>
   );
 }
