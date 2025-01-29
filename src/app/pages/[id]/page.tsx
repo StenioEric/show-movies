f028afe2f04fa06ad1886bd79d6d659eb7b10541
StenioEric <stenioeric@gmail.com>
2025-01-29 10:30:47 -0300

    Alteração na requisição para axios

--- src/app/pages/[id]/page.tsx
+++ src/app/pages/[id]/page.tsx
@@ -1,3 +1,4 @@
+import axios from "axios"; // Importa o Axios para fazer a requisição
 import { Movie } from "@/type/movie"; // Importa tipagem do filme
 import StarRating from "@/StarRating"; // Componente de avaliação por estrelas
 import "./index.scss";
@@ -8,21 +9,31 @@ interface MovieDetailsProps {
   };
 }
 
-// Função para buscar os detalhes do filme na API TMDB
+// Função para buscar os detalhes do filme na API TMDB usando Axios
 async function getMovieDetails(id: string): Promise<Movie> {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=4e1ab5dc8540ca07b5002ed46eb494e7&language=pt-BR`
-  ).then((res) => res.json()); // Converte a resposta para JSON
-
-  return res as Movie;
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${id}`,
+      {
+        params: {
+          api_key: "4e1ab5dc8540ca07b5002ed46eb494e7",
+          language: "pt-BR",
+        },
+      }
+    );
+    return response.data as Movie;
+  } catch (error) {
+    console.error("Erro ao buscar detalhes do filme:", error);
+    throw new Error("Filme não encontrado.");
+  }
 }
 
 export default async function MovieDetails({ params }: MovieDetailsProps) {
   // Busca os detalhes do filme com base no ID recebido
-  const movie = await getMovieDetails(params.id);
-
-  // Verifica se o filme não foi encontrado
-  if (!movie) {
+  let movie: Movie;
+  try {
+    movie = await getMovieDetails(params.id);
+  } catch {
     return <div className="error-message">Filme não encontrado.</div>;
   }
 
